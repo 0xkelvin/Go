e762215dfd78375913d590c7a08a5fcbf871bdf9
vietmaiquoc <quocvm1@gmail.com>
2021-10-26 23:14:14 +0800

    [Viet] day 5 of study go A tour of Go

--- Defer_Panic_Recover.go
+++ Defer_Panic_Recover.go
@@ -84,3 +84,41 @@ all functions in the curent goroutine have returned, at which point the program 
 by invoking panic directly. They can also be caused by runtime errors, such as out-of-bounds array accesses
 
 */
+//----------------------------------------------------------------------------------------------
+// Recover 
+/*
+ is a built-in function that regains control of a panicking goroutine. Recover is only useful inside 
+ deferred functions. During normal execution, a call to recover will return nil and have no other effect
+ If the current goroutine is panicking, a call to recover will capture the value given to panic and resume
+ normal execution.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	f()
+	fmt.Println("Returned normally from f.")
+}
+
+func f() {
+	defer func() {
+		if r:= recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v",i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i+1)
+}
